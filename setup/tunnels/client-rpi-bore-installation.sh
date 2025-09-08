2e41be7e8e3b4be3c2f9ec270fccec1c63b29cb4
tacoverflow <76685046+tacoverflow@users.noreply.github.com>
2025-09-08 13:34:06 -0700

    Update client-rpi-bore-installation.sh

--- setup/tunnels/client-rpi-bore-installation.sh
+++ setup/tunnels/client-rpi-bore-installation.sh
@@ -1,6 +1,8 @@
 # get latest version url of bore from the main repo  (https://github.com/ekzhang/bore)
-BORE_URL=$(curl -s https://api.github.com/repos/ekzhang/bore/releases/latest | jq '.assets[] | select(.name | contains("linux"|"arm-"|"musleabi.")).browser_download_url')
+BORE_URL=$(curl -s https://api.github.com/repos/ekzhang/bore/releases/latest | jq -r '.assets[] | select(.name | contains("linux") and contains("arm-") and contains("musleabi.")).browser_download_url')
 # install latest version 
 wget $BORE_URL
+# untar archive
+tar -xvf $(basename $BORE_URL)
 # move the bin to /usr/local/bin
 mv ./bore /usr/local/bin/bore
