23edf31effcf2fb7f81bc8c9a4f7ff6d3623c69d
tacoverflow <76685046+tacoverflow@users.noreply.github.com>
2025-09-08 13:32:37 -0700

    Create server-bore-installation.sh

---
+++ setup/tunnels/server-bore-installation.sh
@@ -0,0 +1,10 @@
+# install dependencies 
+sudo DEBIAN_FRONTEND=noninteractive apt install jq -y
+# get latest version url of bore from the main repo  (https://github.com/ekzhang/bore)
+BORE_URL=$(curl -s https://api.github.com/repos/ekzhang/bore/releases/latest | jq -r '.assets[] | select(.name | contains("linux") and contains("i686") and contains("musl.")).browser_download_url')
+# install latest version 
+wget $BORE_URL
+# untar archive
+tar -xvf $(basename $BORE_URL)
+# move the bin to /usr/local/bin
+mv ./bore /usr/local/bin/bore
