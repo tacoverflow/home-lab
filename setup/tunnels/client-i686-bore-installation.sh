e1564c7935374361e0e4fa0fb30f0423ee7708a3
tacoverflow <76685046+tacoverflow@users.noreply.github.com>
2025-09-09 20:03:28 -0700

    Create client-i686-bore-installation.sh

---
+++ setup/tunnels/client-i686-bore-installation.sh
@@ -0,0 +1,10 @@
+# get latest version url of bore from the main repo  (https://github.com/ekzhang/bore)
+BORE_URL=$(curl -s https://api.github.com/repos/ekzhang/bore/releases/latest | jq -r '.assets[] | select(.name | contains("linux") and contains("i686") and contains("musl.")).browser_download_url')
+# install latest version 
+wget $BORE_URL
+# untar archive
+tar -xvf $(basename $BORE_URL)
+# move the bin to /usr/local/bin
+sudo mv ./bore /usr/local/bin/bore
+# delete old tar archive
+rm $(basename $BORE_URL)
