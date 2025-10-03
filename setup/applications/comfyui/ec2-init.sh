e5d1a5c9e2ed6d1dfd3ff57e4559a6076bbd81c9
tacoverflow <76685046+tacoverflow@users.noreply.github.com>
2025-10-03 01:09:46 -0700

    Update ec2-init.sh

--- setup/applications/comfyui/ec2-init.sh
+++ setup/applications/comfyui/ec2-init.sh
@@ -1,3 +1,23 @@
+# mount persistent efs
 sudo yum install -y amazon-efs-utils
 sudo mkdir efs
 sudo mount -t efs $EFS_ID efs/
+
+# install bore
+curl -s https://raw.githubusercontent.com/tacoverflow/home-lab/refs/heads/main/setup/tunnels/client-i686-bore-installation.sh | bash
+
+# create bore secret
+echo "$BORE_PASS" | sudo tee /etc/systemd/system/bore.pass
+sudo chmod 600 /etc/systemd/system/bore.pass
+sudo chown root:root /etc/systemd/system/bore.pass
+
+# create a bore service to connect to server
+sudo curl -o /etc/systemd/system/bore-client@.service https://raw.githubusercontent.com/tacoverflow/home-lab/refs/heads/main/setup/tunnels/systemd/bore-client%40.service
+EXPOSE_PORTS="comfyui 8188 8188"
+echo "$EXPOSE_PORTS" | while read tunnel_name local_port remote_port
+do
+    echo "BORE_LOCAL_PORT=$local_port" | sudo tee /etc/systemd/system/bore-${tunnel_name}.conf
+    echo "BORE_REMOTE_PORT=$remote_port" | sudo tee -a /etc/systemd/system/bore-${tunnel_name}.conf
+    sudo systemctl enable bore-client@${tunnel_name}.service
+    sudo systemctl start bore-client@${tunnel_name}.service
+done
