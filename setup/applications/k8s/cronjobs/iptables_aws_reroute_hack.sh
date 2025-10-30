b39d9279d3038531a5e78809094d395c3d10ca06
tacoverflow <76685046+tacoverflow@users.noreply.github.com>
2025-10-29 17:29:18 -0700

    Update iptables_aws_reroute_hack.sh

--- setup/applications/k8s/cronjobs/iptables_aws_reroute_hack.sh
+++ setup/applications/k8s/cronjobs/iptables_aws_reroute_hack.sh
@@ -1,14 +1,16 @@
 #! /bin/bash -xe
 # Place on /opt/iptables_aws_reroute_hack.sh
-
-#sudo iptables -X
-
-export AWS_PROFILE=terraform
-ip_pairs=$(aws ec2 describe-instances \
+ip_pairs=$(/usr/local/bin/aws ec2 describe-instances \
         --filters "Name=tag:Name,Values=k8s-worker" "Name=instance-state-name,Values=running" \
         --query "Reservations[].Instances[][PrivateIpAddress,PublicIpAddress]" \
         --output text)
 
-echo "$ip_pairs" | while read internal_ip external_ip; do
-        sudo iptables -t nat -A OUTPUT -p tcp --dport 10250  -d $internal_ip -j DNAT --to-destination $external_ip
-done
+[ -z  "$ip_pairs" ] || echo "$ip_pairs" | while read internal_ip external_ip; do
+        iptables -C OUTPUT -t nat -p tcp --dport 10250  -d $internal_ip -j DNAT --to-destination $external_ip 2>/dev/null
+        if [ $? -eq 1 ]; then
+                iptables -t nat -A OUTPUT -p tcp --dport 10250  -d $internal_ip -j DNAT --to-destination $external_ip
+                echo "Adding: iptables -t nat -A OUTPUT -p tcp --dport 10250  -d $internal_ip -j DNAT --to-destination $external_ip"
+        else
+                echo "Skipping: iptables -t nat -A OUTPUT -p tcp --dport 10250  -d $internal_ip -j DNAT --to-destination $external_ip"
+        fi
+don
