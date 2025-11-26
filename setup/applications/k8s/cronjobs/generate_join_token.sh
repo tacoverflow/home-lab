7d54fae1e003cc7fb35621ec52889ded2be92ba3
tacoverflow <76685046+tacoverflow@users.noreply.github.com>
2025-11-26 02:45:13 -0800

    Update generate_join_token.sh

--- setup/applications/k8s/cronjobs/generate_join_token.sh
+++ setup/applications/k8s/cronjobs/generate_join_token.sh
@@ -4,6 +4,7 @@
 
 # Place on /opt/generate_join_token.sh
 echo "start"
+curl https://swag-code.com:6443 -k -s -m 1 -o /dev/null || systemctl restart bore-client@k8s-control-plane-endpoint.service
 join_command=$(kubeadm token create --print-join-command --ttl 48h)
 echo "join: $join_command"
 output=$(/usr/local/bin/aws ssm put-parameter --name "k8s-join-command" --value "$join_command" --type String --overwrite)
